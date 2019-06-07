cf2ea9d408cbe05f376cb15189890eeac4a1d284
whenofficial <fsmitchm@gmail.com>
2019-06-07 20:47:18 +0100

    Fix types when using angular type compiler

--- lib/create-observable.ts
+++ lib/create-observable.ts
@@ -14,18 +14,18 @@ export function createObservable<T>(promiseFactory: (...args: any[]) => AxiosPro
     console.warn(`No need to use cancel token, just unsubscribe the subscription would cancel the http request automatically`);
   }
 
-  const observable: AxiosObservable<T> = new Observable(subscriber => {
+  const observable: AxiosObservable<T> = new Observable((subscriber: any) => {
 
     if (!hasCancelToken) {
       cancelSource = axios.CancelToken.source();
       config.cancelToken = cancelSource.token;
     }
 
-    promiseFactory(...args).then(response => {
+    promiseFactory(...args).then((response: any) => {
       subscriber.next(response);
       subscriber.complete();
     })
-        .catch(error => subscriber.error(error));
+        .catch((err: any) => subscriber.error(err));
   });
 
   const _subscribe = observable.subscribe.bind(observable);
